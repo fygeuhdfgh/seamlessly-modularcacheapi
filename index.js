06481ecd2d1e5cc82411f927bb8b0320e4dac332
fygeuhdfgh <tsjcom.gregorio@hotmail.com>
2024-05-12 02:49:06 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const gnomeSort = (arr) => {
-  let index = 0;
-  while (index < arr.length) {
-    if (index === 0 || arr[index] >= arr[index - 1]) {
-      index++;
-    } else {
-      [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
-      index--;
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
   }
-  return arr;
-};
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
