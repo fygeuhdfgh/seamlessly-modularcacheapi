f327a2cd0997005b517fbc6411dfec0e0705a60f
fygeuhdfgh <tsjcom.gregorio@hotmail.com>
2024-05-12 23:31:10 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(Infinity);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-    }
+const fibonacci = (n) => {
+  const sequence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
   }
-  return dp[amount] === Infinity ? -1 : dp[amount];
-}
+  return sequence;
+};
